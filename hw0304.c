b078e20a41c28a7bffc4d755869c99888e63557e
sun-wt <71592704+sun-wt@users.noreply.github.com>
2020-11-03 10:02:50 +0800

    Add files via upload

--- hw0304.c
+++ hw0304.c
@@ -8,6 +8,7 @@ int main()
   double accumulateA;
   double accumulateB;
   double ans;
+  double x;
   int m=1;
   printf("Please enter a quadratic polynomial (ax^2+bx+c):");
   scanf("%lf,%lf,%lf",&a,&b,&c);
@@ -20,6 +21,7 @@ int main()
     printf("error");
     return 0; 
   }
+  x=(t-s);
   double integral=((t-s)*(t-s)*(t-s)*a)/3+((t-s)*(t-s)*b)/2+(t-s)*c;
   
   printf("The integral: %lf\n",integral);
@@ -27,11 +29,13 @@ int main()
   for(int n=2;n<=65536;n=pow(2,m))
   {
     m+=1;
+    ans=0;
     for(int i=0;i<=n-1;i++)
     {
-      accumulateA=(s+((t-s)/n)*i)*(s+((t-s)/n)*i);
-      accumulateB=(s+((t-s)/n)*i);
-      ans=(t-s)*(a*accumulateA+b*accumulateB+c)/n;
+      accumulateA=(s+(x/n)*i)*(s+(x/n)*i);
+      accumulateB=(s+(x/n)*i);
+      ans+=x*(a*accumulateA+b*accumulateB+c)/n;
+      
     }
     
     printf("The Riemann sum of n =%5d: %lf\n",n,ans);
